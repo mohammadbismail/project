d9310ee1c1afc64c4707704f75724e3ea24db519
mohammadbisamil <m.basim.ismail@gmail.com>
2022-10-09 21:39:11 +0300

    update

--- rental_app/models.py
+++ rental_app/models.py
@@ -1,3 +1,4 @@
+from tkinter import CASCADE
 from django.db import models
 
 
@@ -12,12 +13,14 @@ class User(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
 # each user can be a customer optionally if he booked a car
 class Customer(models.Model):
-    user = models.OneToOneField(User, primary_key=True)
+    user = models.OneToOneField(User, primary_key=True,on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     # cars_booked
+
 
 class Provider(models.Model):
     name = models.CharField(max_length=50)
@@ -26,33 +29,34 @@ class Provider(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     # cars_provided
-    #customer_feedbacks
+    # customer_feedbacks
+
 
 # car can be booked by many customers at different times, while
 # customer can book many cars at once
 class Car(models.Model):
     customers = models.ManyToManyField(Customer, related_name="cars_booked")
-    provider = models.ForeignKey(Provider, related_name="cars_provided")
+    provider = models.ForeignKey(Provider, related_name="cars_provided",on_delete=models.CASCADE)
     brand = models.CharField(max_length=45)
     model = models.CharField(max_length=45)
     plate_number = models.CharField(max_length=45)
     production_year = models.IntegerField()
     engine = models.IntegerField()
-    pickup_date = models.DateTimeField(null=True) #this field can be empty unless while creating car, unless its booked
+    pickup_date = models.DateTimeField(
+        null=True
+    )  # this field can be empty unless while creating car, unless its booked
     drop_date = models.DateTimeField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    #feedbacked_cars
+    # feedbacked_cars
+
+
+# car can have multiple feedbacks - a feedback can be for mutiple cars
 
 class Feedback(models.Model):
     details = models.TextField(null=True)
-    customer = models.ForeignKey(Customer,related_name="customer_feedbacks")
+    customer = models.ForeignKey(Customer, related_name="customer_feedbacks",on_delete=models.CASCADE)
     rating = models.IntegerField()
-    cars = models.ManyToManyField(Car,related_name="feededbacked_cars")
+    cars = models.ManyToManyField(Car, related_name="feededbacked_cars")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-
-
-
-
